bf7cdd4a5ffd2bdda164fe963d8a81f260d6f2f8
adriibanez7 <adrian.ibanez@circulargo.com>
2023-11-14 11:42:27 +0100

    commit tema 5 ej 2

--- application/controllers/Listado.php
+++ application/controllers/Listado.php
@@ -7,11 +7,9 @@ class Listado extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->model('ProductosModel');
+		$this->load->helper('string_helper');
 		$this->load->helper('html');
-		$this->load->helper('url');
-		$this->load->helper('form');
 		$this->load->library('table');
-		$this->load->library('pagination');
 	}
 
 	public function index()
@@ -19,33 +17,16 @@ class Listado extends CI_Controller {
 		$this->mostrar_listado();
 	}
 
-	public function mostrar_listado($num_registros = 5)
+	public function mostrar_listado()
 	{
-		if ($this->input->post('num_registros')) {
-			$num_registros = $this->input->post('num_registros');
-		}
-		$config = array();
-		$config["base_url"] = base_url() . "listado/mostrar_listado";
-		$config["total_rows"] = $this->ProductosModel->get_products_count();
-		$config["per_page"] = $num_registros;
-		$config["uri_segment"] = 3;
-		$config['first_link'] = '<<';
-		$config['last_link'] = '>>';
-		$config['next_link'] = '>';
-		$config['prev_link'] = '<';
+		$data['listado'] = array(
+			'vacio'=>'',
+			'null'=>null,
+			'fecha0'=>'0000-00-00, 0000-00-00 00:00:00',
+			'patatas'=>'patatas',
+			'peras'=>'peras'
+		);
 
-		$this->pagination->initialize($config);
-
-		$page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
-		$data["productos"] = $this->ProductosModel->get_products_and_categories($config["per_page"], $page);
-		$data["links"] = $this->pagination->create_links();
-
-		$start = $page + 1;
-		$end = min(($page + 1) * $config["per_page"], $config["total_rows"]);
-		$total = $config["total_rows"];
-		$data["pagination_text"] = "{$start} al {$end} de {$total} productos";
-
-		$data["num_registros_selected"] = $num_registros;
 
 		$this->load->view('listado', $data);
 	}
--- application/views/listado.php
+++ application/views/listado.php
@@ -1,32 +1,12 @@
 <div class="container">
-	<h2>Listado de todos los Productos</h2>
-	<?php $this->table->set_heading(array('ID', 'PRODUCTO', 'MARCA', 'CATEGORIA','CANTIDAD','PRECIO')); ?>
-
-	<?php foreach ($productos as $producto): ?>
-		<?php
-		$detalles_url = site_url('ficha/mostrar_ficha/' . $producto['PK_ID_PRODUCTO']);
-		?>
-		<?php $this->table->add_row(
-			anchor(site_url("ficha/mostrar_ficha/"). $producto['PK_ID_PRODUCTO'],$producto['PK_ID_PRODUCTO']),
-			$producto['NOMBRE_PRODUCTO'],
-			$producto['MARCA'],
-			$producto['NOMBRE_CATEGORIA'],
-			$producto['CANTIDAD'] . ' uds.',
-			$producto['PRECIO']. ' €'
-		); ?>
+	<h2>Listado</h2>
+		<?php $this->table->set_heading(array('CLAVE', 'VALOR_INICIAL','VALOR_FINAL')); ?>
+	<?php foreach ($listado as $clave => $valor): ?>
+<!--		--><?php //var_dump($listado); var_dump(alt_text($listado['vacio']));?>
+		<?php $this->table->add_row($clave,$valor,alt_text($valor)); ?>
 	<?php endforeach; ?>
 
 	<?= $this->table->generate(); ?>
 
-	<div class="pagination">
-		<?php echo $links; ?>
-	</div>
 
-	<p><?= $pagination_text; ?></p>
-
-	<?php
-	echo form_open('listado/mostrar_listado', array('id' => 'form_num_registros'));
-	echo form_dropdown_num_records('num_registros', $num_registros_selected, 'form_num_registros');
-	echo form_close();
-	?>
 </div>
